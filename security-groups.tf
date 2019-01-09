0040ca68764765a5ddad07fddc0df97a685496f3
sathish-reddimalla <46507701+sathish-reddimalla@users.noreply.github.com>
2019-01-09 14:18:19 -0500

    Removed public access

--- security-groups.tf
+++ security-groups.tf
@@ -25,20 +25,6 @@ resource "aws_security_group" "web" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 	
-	ingress {
-        protocol    = "tcp"
-        from_port   = 22
-        to_port     = 22
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-	
-	ingress {
-        protocol    = "-1"
-        from_port   = 0
-        to_port     = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-	
     tags {
         Name = "${var.aws_env}-${var.aws_name}-sg-web"
     }
