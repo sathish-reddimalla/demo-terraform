e761d85ef5ce9adcd62c915b772cf4bfd51a9ed1
sathish-reddimalla <46507701+sathish-reddimalla@users.noreply.github.com>
2019-01-09 13:37:43 -0500

    Update instance-profile.tf

--- instance-profile.tf
+++ instance-profile.tf
@@ -2,6 +2,6 @@
 # Instance Profile
 #
 resource "aws_iam_instance_profile" "ecs" {
-    name = "${var.aws_env}-${var.aws_name}-instance-profiles"
+    name = "${var.aws_env}-${var.aws_name}-instance-profiles1"
     role = "${aws_iam_role.ecs_instance_role.name}"
 }
